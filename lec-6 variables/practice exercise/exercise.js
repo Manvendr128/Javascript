708a0164727f88ca17b191597915a4dd534796e1
Manvendra Singh <manvendrasingh2260@gmail.com>
2026-06-04 20:50:13 +0530

    all file has been commit

---
+++ lec-6 variables/practice exercise/exercise.js
@@ -0,0 +1,6 @@
+const myname = "Manvendra Singh";
+console.log(eval('50+90')); 
+document.querySelector('#my-name').innerText=myname;
+const gst = 18;
+
+let cartvalue = Math.round(((15599-(10/100*15599))-272 + (2499-(41/100*2499))-29+25+20)*(100+gst)/100);
